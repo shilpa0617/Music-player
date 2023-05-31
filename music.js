e51f362ccedfe3d0507954c831fe441197f0c3a4
shilpa0617 <104649676+shilpa0617@users.noreply.github.com>
2023-05-31 15:11:27 +0530

    first commit

---
+++ music.js
@@ -0,0 +1,32 @@
+let trackList = [
+  {
+    name: "Maan Meri Jaan",
+    path: "music/music1.mp3",
+    img: "images/1.jpg",
+    singer: "King",
+  },
+  {
+    name: "Chal Ghar Chalen",
+    path: "music/music2.mp3",
+    img: "images/2.jpg",
+    singer: "Arijit Singh",
+  },
+  {
+    name: "Tere Sang Yaara",
+    path: "music/music3.mp3",
+    img: "images/3.jpg",
+    singer: "Atif Aslam",
+  },
+  {
+    name: "Shiddat Title Track",
+    path: "music/music4.mp3",
+    img: "images/4.jpg",
+    singer: "Manan Bhardwaj",
+  },
+  {
+    name: "Tere Bin",
+    path: "music/music5.mp3",
+    img: "images/5.jpg",
+    singer: "Rahat Fateh Ali Khan, Asees Kaur, Tanishk Bagchi",
+  },
+];
